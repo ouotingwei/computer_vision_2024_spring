67aaa313906a6b0756a1aa1264ea3d7424717705
tingweiou <kklb1716@gmail.com>
2024-05-04 18:22:25 +0800

    debug ok

--- CV_HW2_2024/HW2.py
+++ CV_HW2_2024/HW2.py
@@ -52,7 +52,7 @@ def find_sift_kp_and_des( img_gray ):
 
     return kp, des
 
-def cylinder_projection( img, focal=4000 ):
+def cylinder_projection( img, focal=2200 ):
     h, w = img.shape[:2]
     cylinder_projection_img = np.zeros_like( img )
     center_x, center_y = w//2, h//2
@@ -81,46 +81,78 @@ def crop_img( img, crop_ratio=1):
 
     return cropped_img
 
+
+def calculate_average_brightness(img_list):
+    total_brightness = 0
+    
+    for img_path in img_list:
+        img = cv.imread(img_path)
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        current_brightness = cv.mean(gray)[0]
+        total_brightness += current_brightness
+    
+    avg_brightness = total_brightness / len(img_list)
+    
+    return avg_brightness
+
+def auto_adjust_brightness(image, target_brightness):
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    current_brightness = cv.mean(gray)[0]
+    ratio = target_brightness / current_brightness
+    adjusted_image = cv.convertScaleAbs(image, alpha=ratio, beta=0)
+    
+    return adjusted_image
+
 if __name__ == '__main__':
     if not os.path.exists(file_path):
         print("The file path does not exist.")
         exit()
-
+    
     img_file = os.listdir(file_path)
     img_file.sort()
     print(img_file)
     files_cnt = len(img_file)
-
-    for i in range( files_cnt-1 ):
-        img_left = None
-        img_left_gray = None
+    
+    img_paths = [os.path.join(file_path, img.split('.')[0] + ".jpg") for img in img_file]
+    
+    # calculate the average brightness
+    avg_brightness = calculate_average_brightness(img_paths)
+    print("Average brightness:", avg_brightness)
+    
+    # adjust brightness
+    img_left = None
+    img_left_gray = None
+    
+    img_left_path = img_paths[0]
+    img_left = cv.imread(img_left_path)
+    img_left = auto_adjust_brightness(img_left, avg_brightness)
+    img_left_gray = cv.cvtColor(img_left, cv.COLOR_BGR2GRAY)
+    
+    for i in range(files_cnt - 1):
         img_right = None
         img_right_gray = None
 
-        if i == 0:
-            img_left_path = os.path.join(file_path, img_file[0].split('.')[0] + ".jpg")
-            img_right_path = os.path.join(file_path, img_file[1].split('.')[0] + ".jpg")
-            img_left, img_left_gray = read_img( img_left_path, False )
-            img_right, img_right_gray = read_img( img_right_path, False )
-        else:
-            img_left_path = os.path.join("CV_HW2_2024/Photos/Base/result_image.jpg")
-            img_right_path = os.path.join(file_path, img_file[i+1].split('.')[0] + ".jpg")
-            img_left, img_left_gray = read_img( img_left_path, False )
-            img_right, img_right_gray = read_img( img_right_path, False )
-        
+        img_right_path = img_paths[i + 1]
+        img_right = cv.imread(img_right_path)
+        img_right = auto_adjust_brightness(img_right, avg_brightness)
+        img_right_gray = cv.cvtColor(img_right, cv.COLOR_BGR2GRAY)
+
         print("left:", img_left_path)
         print("right:", img_right_path)
-    
-        img_left_kp, img_left_des = find_sift_kp_and_des( img_left_gray )
-        img_right_kp, img_right_des = find_sift_kp_and_des( img_right_gray )
-    
+
+        img_left_kp, img_left_des = find_sift_kp_and_des(img_left_gray)
+        img_right_kp, img_right_des = find_sift_kp_and_des(img_right_gray)
+
         matcher = si_o.feature_match(img_left, img_right, img_left_kp, img_right_kp, img_left_des, img_right_des)
-        Homography_matrix = matcher.frame_match( MY_FUNCTION=False )
+        Homography_matrix = matcher.frame_match(MY_FUNCTION=False)
         stitched_img = st_o.stitch_img(img_left, img_right, Homography_matrix)
-    
+
         result_image_bgr = (stitched_img * 255).astype(np.uint8)
         result_image_bgr = removeBlackBorder(result_image_bgr)
 
-        cropped_img = crop_img( result_image_bgr )
-        
+        cropped_img = crop_img(result_image_bgr)
+
         cv.imwrite('/home/wei/computer_vision_2024_spring/CV_HW2_2024/Photos/Base/result_image.jpg', cropped_img)
+
+        img_left = cropped_img
+        img_left_gray = cv.cvtColor(img_left, cv.COLOR_BGR2GRAY)
